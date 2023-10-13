7e8a9b4ebe53e695a55b7079f1eb018f0bf1ed57
EC2 Default User <ec2-user@ip-172-31-34-165.ap-southeast-2.compute.internal>
2023-10-13 04:44:12 +0000

    修正版

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-let result = document.getElementById("result");
 
 function clickButton(target) {
+    let result = document.getElementById("result");
     let target_value = target.innerHTML;
+    let array = ['+','-','*','/','.'];
     
-    if (target_value == "AC") {
+    if (target_value == "AC") {　//ACを押したときに０になる
         result.value = 0;
-    } else if (target_value == "=") {
+    } else if (target_value == "=") {　//=を押したときにevalの中身を計算する
         result.value = eval(result.value);
-    } else if (result.value == 0 )
-           if (target_value == "+"
-           || target_value == "-"
-           || target_value == "*"
-           || target_value == "/"
-           || target_value == ".") {
+    } else if (result.value == 0 )　//displayのところが0のとき「+-*/.」を押しても０のまま
+           if (array.includes(target_value)) {
               target_value == "0";
            } else {
                result.value = target_value;
@@ -21,11 +18,7 @@ function clickButton(target) {
                    || result.value.slice(-1) == "*"
                    || result.value.slice(-1) == "/"
                    || result.value.slice(-1) == ".") {
-                       if (target_value == "+"
-                       || target_value == "-"
-                       || target_value == "*"
-                       || target_value == "/"
-                       || target_value == ".") {
+                       if (array.includes(target_value)) {
                           target_value == "";
                        } else {
                            result.value += target_value;
